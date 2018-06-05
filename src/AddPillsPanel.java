bab46e41160a00b262dfe6e6d4a3882f19b38337
Gilabend <gilabend.yann@gmail.com>
2018-06-05 09:57:52 +0200

    added a class and some changes

---
+++ src/AddPillsPanel.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt*;
+
+public class AddPillsPanel extends JPanel {
+	
+	private JComboBox med;
+	private JCheckBox lundi;
+	private JCheckBox mardi;
+	private JCheckBox mercredi;
+	private JCheckBox jeudi;
+	private JCheckBox vendredi;
+	private JCheckBox samedi;
+	private JCheckBox dimanche;
+	private JCheckBox matin;
+	private JCheckBox midi;
+	private JCheckBox soir;
+	private JCheckBox coucher;
+	private JButton add;
+	private JCheckBox end;
+	
+	
+	public AddPills() {
+		initComponents();
+	}
+	
+	public void initComponents() {
+		
+		
+	}

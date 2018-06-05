5c776d8d4441c1ea5eebeb616050f7a3e4493c12
Deilyora <deilyora@localhost.localdomain>
2018-06-05 10:03:08 +0200

    yolo

--- src/AddPillsPanel.java
+++ src/AddPillsPanel.java
@@ -2,10 +2,10 @@ package app;
 
 import java.util.*;
 import javax.swing.*;
-import java.awt*;
+import java.awt.*;
 
 public class AddPillsPanel extends JPanel {
-	
+
 	private JComboBox med;
 	private JCheckBox lundi;
 	private JCheckBox mardi;
@@ -20,13 +20,13 @@ public class AddPillsPanel extends JPanel {
 	private JCheckBox coucher;
 	private JButton add;
 	private JCheckBox end;
-	
-	
+
+
 	public AddPills() {
 		initComponents();
 	}
-	
+
 	public void initComponents() {
-		
-		
+
+
 	}
--- src/ConnectionPanel.java
+++ src/ConnectionPanel.java
@@ -2,22 +2,20 @@ package app;
 
 import java.awt.*;
 import java.io.*;
-import javax.swing
+import javax.swing.*;
 
 public class ConnectionPanel extends Jpanel {
 	private JLabel title;
 	private JTextField text;
 	private JButton connect;
 	private JButton back;
-	
+
 	public ConnectionPanel() {
 		initComponents();
 	}
-	
+
 	public void initComponents() {
 		title = new JLabel("Paramètres");
 		text = new JTextField("Mot de passe");
 		connect = new JButton("Se connecter");
 		back = new JButton("Retour au menu");
-		
-		

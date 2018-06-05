0853db4e71279892be085b971e59814223f779a4
Gilabend <gilabend.yann@gmail.com>
2018-06-05 11:57:29 +0200

    xD

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -18,16 +18,7 @@ public class MainFrame extends JFrame {
 		LocationPanel lp = new LocationPanel();
 		FindingPillsPanel fpp = new FindingPillsPanel();
 		PillBoxPanel pbp = new PillBoxPanel();
-<<<<<<< HEAD
-		StoringPillsPanel = new StoringPillsPanels();
-		JobDonePanel = new JobDonePanel();
-		ConnectionPanel = new ConnectionPanel();
-		*/
-		AddPillsPanel app= new AddPillsPanel();
 
-		setLayout(new BorderLayout());
-		add(app,BorderLayout.CENTER);
-=======
 		StoringPillsPanel spp = new StoringPillsPanels();
 		JobDonePanel jdp = new JobDonePanel();
 		*/ConnectionPanel cp = new ConnectionPanel();
@@ -35,7 +26,6 @@ public class MainFrame extends JFrame {
 		add(cp);
 		//setLayout(new BorderLayout());
 		//add(mp,BorderLayout.CENTER);
->>>>>>> a46afe9ca769d4b1a5fcfede296a36944064d523
 		pack();
 	}
 
